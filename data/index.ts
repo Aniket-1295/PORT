e69f1a8d13da72c6b02bbbcebd9e2d9a6aa2ce8a
Aniket <you@example.com>
2024-12-29 15:06:38 +0530

    Done

---
+++ data/index.ts
@@ -0,0 +1,179 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I consistently deliver exceptional results.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Working on FullStack Projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to Connect with me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    des: "A Personal Portfolio Website.Made using NextJs and Typescript",
+    img: "/PortPhoto2.PNG",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Qkart-frontend",
+    des: "An E-commerce website made using React and Material UI",
+    img: "/qartPhoto.PNG",
+    iconLists: ["/re.svg"],
+    link: "https://qkart-frontend-theta-vert.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "QTify",
+    des: "QTify is a song-browsing application.made using React",
+    img: "/qtfyPhoto.PNG",
+    iconLists: ["/re.svg"],
+    link: "https://q-tify-blush-phi.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "MEDIFY",
+    des: "MEDIFY is a React-based web application that allows user to find medical centers and book appointments.",
+    img: "/medfyPhoto.PNG",
+    iconLists: ["/re.svg"],
+    link: "https://mdify.vercel.app/",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "MongoDB",
+    img: "/mongoPhoto5.jpg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "Express.js",
+    img: "/ExpressPhoto.png",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "React.Js",
+    img: "/ReactPhoto2.png",
+    // nameImg: "/re.svg",
+  },
+  {
+    id: 4,
+    name: "Node.Js",
+    img: "/nodePhoto.png",
+    nameImg: "/streamName.svg",
+  },
+ 
+];
+
+export const workExperience = [
+
+  {
+    id: 1,
+    title: "Crio.do",
+    desc: "08 2024 - Present",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Pimpri Chinchwad College of Engineering ,Pune",
+    desc: "08 2020 – 06 2024",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Sadhana jr. College of Science",
+    desc: "08 2019 – 06 2020",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Sau.V.D.Shinde HighSchool,Gadhinglaj",
+  //   desc: "Percentage - 90.40%",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/Aniket-1295",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/aniket-kamble-865694214",
+  },
+];
